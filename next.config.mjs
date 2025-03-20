63ea035d57c181ac77f3bf51c6ee9e23ddb48d2f
ekinsirace <ekin.cabuk@gmail.com>
2025-03-20 09:46:18 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "ekinsirace.com" }],
+        destination: "https://ekinsirace.com/:path*",
+        permanent: true, // 301 yönlendirmesi
+      },
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "www.ekinsirace.com" }],
+        destination: "https://ekinsirace.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
